c78712326b01aadf0994f44cd986bd59dbb6273e
Nikhil Rana <harrybite29@gmail.com>
2024-05-31 21:03:06 +0530

    changed factory contract

--- v2-sdk-3.0.0-alpha.2/src/constants.ts
+++ v2-sdk-3.0.0-alpha.2/src/constants.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x1cCF425Ce0E53614CbDBa284c515926a7E45A8B6' // change FACTORY_ADDRESS for testnet or mainnet
+export const FACTORY_ADDRESS = '0x5f0BdbD28B571eF844DD449e2b24392C82cEDEe8' // change FACTORY_ADDRESS for testnet or mainnet
 
-export const INIT_CODE_HASH = '0x47f1546c1597fd39354056525b84d647f10852af88dda77f7b7e1f2520cd8e95' // change INIT_CODE_HASH for testnet or mainnet
+export const INIT_CODE_HASH = '0x239884e3702af0ffbdd92d5d278bd9c573752a3e800ac710ca47301146d3bd0b' // change INIT_CODE_HASH for testnet or mainnet
 // a5d49a9592885b4f8b66115c12564d598b0a0bdce375eb0946695f112d5b45be
 // 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f
 
