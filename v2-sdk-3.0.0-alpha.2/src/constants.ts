fa55280b3843f280d7fbda5c27fbc1e4367b68fe
Nikhil Rana <harrybite29@gmail.com>
2024-06-02 08:53:33 +0530

    changed contract

--- v2-sdk-3.0.0-alpha.2/src/constants.ts
+++ v2-sdk-3.0.0-alpha.2/src/constants.ts
@@ -1,10 +1,10 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5f0BdbD28B571eF844DD449e2b24392C82cEDEe8'// change FACTORY_ADDRESS for testnet or mainnet
+export const FACTORY_ADDRESS = '0x9CbBb8E90E91B9Cf88Fe7F8a6910c0EeFA86c300'// change FACTORY_ADDRESS for testnet or mainnet
 
-export const V2Router02 = "0x6d0c63977df74412B33119928Ba3f1e46CB576f9"
+export const V2Router02 = "0x7Fd6CE5928024bDd49483cf1CA20E69964705aAE"
 
-export const INIT_CODE_HASH = '0x239884e3702af0ffbdd92d5d278bd9c573752a3e800ac710ca47301146d3bd0b' // change INIT_CODE_HASH for testnet or mainnet
+export const INIT_CODE_HASH = '0xc8a0b5d888393b0178d670721ead21a43a4aaef7d36288806ef65f1844dd00db' // change INIT_CODE_HASH for testnet or mainnet
 // a5d49a9592885b4f8b66115c12564d598b0a0bdce375eb0946695f112d5b45be
 // 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f
 
